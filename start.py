3a59383a0297a9c6649046ecac62c9d282036a0e
911_Adam_Paul <72084877+pauladam2001@users.noreply.github.com>
2021-04-10 21:25:11 +0300

    Delete start.py

--- start.py
+++
@@ -1,8 +0,0 @@
-from UserInterface.consoleBased import UI
-from Service.service import Game, RandomSmartMoveStrategy
-
-
-strategy = RandomSmartMoveStrategy()
-game = Game(strategy)
-consoleBased = UI(strategy, game)
-consoleBased.start()
